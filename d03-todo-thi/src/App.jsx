30f69b7729751863c860211e6c3fc05ede4d5d07
Khánh Thi <khanhthi2807@gmail.com>
2022-04-09 02:58:56 +0700

    thi commit todo

--- d03-todo-thi/src/App.jsx
+++ d03-todo-thi/src/App.jsx
@@ -6,6 +6,8 @@ import TodoList from "./views/TodoList";
 
 import "./assets/style.css";
 
+import "./assets/style.css";
+
 function App() {
   // const [formData, setFormData] = useState();
   // const [svArr, setSvArr] = useState([]);
@@ -28,8 +30,70 @@ function App() {
       <Header></Header>
 
       <div className="container mt-5 ">
-        <TodoInput></TodoInput>
-        <TodoList></TodoList>
+        <h2 className="text-center">TodoInput</h2>
+        <div class="flex-container">
+          <div className="input-group">
+            <span>
+              <i class="fa-solid fa-file-lines"></i>
+            </span>
+            <input type="text" id="myInput" placeholder="New Todo" />
+          </div>
+          <button onclick="" class="addBtn">
+            Add
+          </button>
+        </div>
+        <div class=" mt-3 mb-5">
+          <h2 className="text-center">TodoList</h2>
+          <div className="btnTodoList">
+            <button onclick="" class="addBtn btnList">
+              All
+            </button>
+            <button onclick="" class="addBtn btnList">
+              Done
+            </button>
+            <button onclick="" class="addBtn btnList">
+              Todo
+            </button>
+          </div>
+        </div>
+        <ul id="myUL">
+          <li>
+            Learn React Basic
+            <div className="check-form ">
+              <i class="fa-regular fa-square"></i>
+              <i class="fa-solid fa-pen "></i>
+              <i class="fa-solid fa-trash "></i>
+            </div>
+          </li>
+          <li>
+            Learn React Basic
+            <div className="check-form ">
+              <i class="fa-regular fa-square"></i>
+              <i class="fa-solid fa-pen "></i>
+              <i class="fa-solid fa-trash "></i>
+            </div>
+          </li>
+          <li>
+            Learn React Basic
+            <div className="check-form ">
+              <i class="fa-regular fa-square"></i>
+              <i class="fa-solid fa-pen "></i>
+              <i class="fa-solid fa-trash "></i>
+            </div>
+          </li>
+
+          <button type="button" class="btn-close btn-close-white" aria-label="Close"></button>
+        </ul>
+        <div class=" btnDelete ">
+          <div>
+            <button onclick="" class="addBtn btnDel">
+              Delete done tasks
+            </button>
+            <button onclick="" class="addBtn btnDel">
+              Delete all tasks
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
