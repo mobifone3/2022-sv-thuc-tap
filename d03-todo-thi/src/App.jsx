0246c8c9c700fc0c97741d27a844a3fa40237beb
Khánh Thi <khanhthi2807@gmail.com>
2022-04-09 19:40:30 +0700

    thi update

--- d03-todo-thi/src/App.jsx
+++ d03-todo-thi/src/App.jsx
@@ -30,70 +30,8 @@ function App() {
       <Header></Header>
 
       <div className="container mt-5 ">
-        <h2 className="text-center">TodoInput</h2>
-        <div class="flex-container">
-          <div className="input-group">
-            <span>
-              <i class="fa-solid fa-file-lines"></i>
-            </span>
-            <input type="text" id="myInput" placeholder="New Todo" />
-          </div>
-          <button onclick="" class="addBtn">
-            Add
-          </button>
-        </div>
-        <div class=" mt-3 mb-5">
-          <h2 className="text-center">TodoList</h2>
-          <div className="btnTodoList">
-            <button onclick="" class="addBtn btnList">
-              All
-            </button>
-            <button onclick="" class="addBtn btnList">
-              Done
-            </button>
-            <button onclick="" class="addBtn btnList">
-              Todo
-            </button>
-          </div>
-        </div>
-        <ul id="myUL">
-          <li>
-            Learn React Basic
-            <div className="check-form ">
-              <i class="fa-regular fa-square"></i>
-              <i class="fa-solid fa-pen "></i>
-              <i class="fa-solid fa-trash "></i>
-            </div>
-          </li>
-          <li>
-            Learn React Basic
-            <div className="check-form ">
-              <i class="fa-regular fa-square"></i>
-              <i class="fa-solid fa-pen "></i>
-              <i class="fa-solid fa-trash "></i>
-            </div>
-          </li>
-          <li>
-            Learn React Basic
-            <div className="check-form ">
-              <i class="fa-regular fa-square"></i>
-              <i class="fa-solid fa-pen "></i>
-              <i class="fa-solid fa-trash "></i>
-            </div>
-          </li>
-
-          <button type="button" class="btn-close btn-close-white" aria-label="Close"></button>
-        </ul>
-        <div class=" btnDelete ">
-          <div>
-            <button onclick="" class="addBtn btnDel">
-              Delete done tasks
-            </button>
-            <button onclick="" class="addBtn btnDel">
-              Delete all tasks
-            </button>
-          </div>
-        </div>
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
       </div>
     </>
   );
