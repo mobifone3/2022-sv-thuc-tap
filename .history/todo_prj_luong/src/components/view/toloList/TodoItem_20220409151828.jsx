8d87a59178f212755e7105a72abef09d61150af9
kuluong2000 <tranhuuluong156200@gmail.com>
2022-04-10 16:19:19 +0700

    todo project

---
+++ .history/todo_prj_luong/src/components/view/toloList/TodoItem_20220409151828.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../../assets/app.css";
+export default function TodoItem({ idx, name, value, handleCheckBox }) {
+  return (
+    <li className="todo_item">
+      <p className="todo_name">{name}</p>
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          onChange={() => {
+            handleCheckBox(idx, name);
+          }}
+          id="flexCheckDefault"
+        />
+        <i className="fa-solid fa-pen todo_icon_pen"></i>
+        <i className="fa-solid fa-trash todo_icon_remove"></i>
+      </div>
+    </li>
+  );
+}
---
+++ .history/todo_prj_luong/src/components/view/toloList/TodoList_20220409151537.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+export default function TodoList({ todoLists, handleCheckBox, ischeck }) {
+  return (
+    <>
+      {todoLists ? (
+        <ul className="todo_list">
+          {todoLists.map((item, idx) => (
+            <TodoItem key={idx} name={item} idx={idx + 1} ischeck={ischeck || false} handleCheckBox={handleCheckBox}></TodoItem>
+          ))}
+        </ul>
+      ) : null}
+    </>
+  );
+}
